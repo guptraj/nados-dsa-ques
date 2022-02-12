0dbf6cbbdf795cd9a70a3a543816604f5a6f5867
Rajat <guptraj15@gmail.com>
2022-02-12 17:25:42 +0530

    added

---
+++ Recursion/display arrayin reverse/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void takeInput(int[] a, int idx) {
+        if (idx == a.length) {
+            return;
+        }
+        a[idx] = scn.nextInt();
+        takeInput(a, idx + 1);
+        
+    }
+
+    public static void display(int[] a, int idx) {
+        if (idx <0) {
+            return;
+        }
+        System.out.println(a[idx]);
+        display(a, idx - 1);
+        
+    }
+
+    public static void main(String[] args) {
+        
+        int n = scn.nextInt();
+        int[] a = new int[n];
+        takeInput(a, 0);
+        display(a, a.length-1);
+        
+    }
+    
+}
