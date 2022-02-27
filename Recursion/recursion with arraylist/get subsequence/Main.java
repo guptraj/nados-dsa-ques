ba999cc0be2261ef2aa77bddf6ddd3efddf56fb9
Rajat <guptraj15@gmail.com>
2022-02-27 21:01:19 +0530

    added

--- Recursion/recursion with arraylist/get subsequence/Main.java
+++ Recursion/recursion with arraylist/get subsequence/Main.java
@@ -1,3 +1,41 @@
-public class Main {
-    
+import java.util.*;
+
+public class Main
+{
+    public static Scanner scn = new Scanner(System.in);
+
+    public static ArrayList<String> gss(String s)
+    {
+
+        if(s.length()==0)
+        {
+            ArrayList<String> Ans = new ArrayList<>();
+            Ans.add(" ");
+            return Ans;
+        }
+     
+        char c = s.charAt(0);
+        ArrayList<String> Recans = gss(s.substring(1));
+        ArrayList<String> MyAns = new ArrayList<>();
+       
+        for (String ele : Recans)
+        {
+            MyAns.add (ele);
+        }
+
+        for (String ele : Recans)
+        {
+            MyAns.add(c + ele);
+        }
+
+
+
+        return MyAns;
+    }
+    public static void main(String [] args)
+    {
+        String s = scn.nextLine();
+        ArrayList<String> ans = gss(s);
+        System.out.println(ans);
+    }
 }
