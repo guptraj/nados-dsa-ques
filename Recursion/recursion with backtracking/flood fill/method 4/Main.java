0c243df1cd2b90a2b7c41ed229e2cd48ccc0f650
Rajat <guptraj15@gmail.com>
2022-03-06 21:21:37 +0530

    ADDED

---
+++ Recursion/recursion with backtracking/flood fill/method 4/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+// WITH THE USE OF DIRECTIONAL ARRAY AND CHECKING BEFORE CALLING FOR NEXT MOVE
+
+public class Main {
+
+    static int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+    static String[] dirN = { "t", "l", "d", "r" };
+
+    public static void floodFill(int[][] maze, int sr, int sc, String asf) {
+     
+        if (sr == maze.length - 1 && sc == maze[0].length - 1) {
+            System.out.println(asf);
+            return;
+        }
+
+        maze[sr][sc] = 1;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r < maze.length && r >= 0 && c < maze[0].length && c >= 0 && maze[r][c] == 0)
+            {
+                floodFill(maze, r, c, asf + dirN[d]);
+            }
+            
+        }
+        maze[sr][sc] = 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] maze = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                maze[i][j] = scn.nextInt();
+            }
+        }
+
+        floodFill(maze, 0, 0, "");
+    }
+
+}
