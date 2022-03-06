e63294ee858a8411122e351d2f376624ede79b46
Rajat <guptraj15@gmail.com>
2022-03-06 22:42:42 +0530

    added

---
+++ Recursion/recursion with backtracking/target sum subset/Main.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void printTargetSumSubsets(int[] arr, int idx, String asf, int sum, int tar) {
+        if(sum==tar)
+        {
+            System.out.println(asf + ".");
+            return;
+        }
+
+        if(idx ==arr.length)
+        {
+            return;
+        }
+        
+        printTargetSumSubsets(arr, idx+1, asf+ arr[idx] + ", ", sum+arr[idx], tar);
+        printTargetSumSubsets(arr, idx+1, asf, sum, tar);
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = scn.nextInt();
+        }
+
+        int tar = scn.nextInt();
+
+        printTargetSumSubsets(arr, 0, "", 0, tar);
+
+    }
+
+
+}
