977a6d2cbfcd995a6d636b71112e0faf38f3f98b
Rajat <guptraj15@gmail.com>
2022-08-26 16:43:04 +0530

    added

--- 08.dp/goldmine/Main.java
+++ 08.dp/goldmine/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    public static int goldmine(int r, int c, int[][] grid, int[][] dp) {
+    public static int goldmineM(int r, int c, int[][] grid, int[][] dp) {
         if (r < 0 || r >= grid.length || c >= grid[0].length) {
             return 0;
         }
@@ -12,13 +12,46 @@ public class Main {
             return dp[r][c];
         }
 
-        int op1 = goldmine(r - 1, c + 1, grid, dp);
-        int op2 = goldmine(r, c + 1, grid, dp);
-        int op3 = goldmine(r + 1, c + 1, grid, dp);
+        int op1 = goldmineM(r - 1, c + 1, grid, dp);
+        int op2 = goldmineM(r, c + 1, grid, dp);
+        int op3 = goldmineM(r + 1, c + 1, grid, dp);
 
         dp[r][c] = Math.max(op1, Math.max(op2, op3)) + grid[r][c];
 
         return dp[r][c];
+    }
+
+    public static int goldmineT(int[][] grid, int[][] dp) {
+        for (int j = grid[0].length - 1; j >= 0; j--) {
+            for (int i = 0; i < grid.length; i++) {
+                if (j == grid[0].length - 1) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    int op1 = dp[i][j + 1];
+                    int op2 = dp[i + 1][j + 1];
+
+                    dp[i][j] = grid[i][j] + Math.max(op1, op2);
+                } else if (i == grid.length - 1) {
+                    int op1 = dp[i][j + 1];
+                    int op2 = dp[i - 1][j + 1];
+
+                    dp[i][j] = grid[i][j] + Math.max(op1, op2);
+                } else {
+                    int op1 = dp[i][j + 1];
+                    int op2 = dp[i - 1][j + 1];
+                    int op3 = dp[i + 1][j + 1];
+
+                    dp[i][j] = grid[i][j] + Math.max(op1, Math.max(op2, op3));
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i][0] > ans)
+                ans = dp[i][0];
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) throws Exception {
@@ -33,14 +66,14 @@ public class Main {
         }
 
         int[][] dp = new int[n][m];
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int temp = goldmine(i, 0, grid, dp);
-            if (temp > ans)
-                ans = temp;
-        }
+        // int ans =0;
+        // for(int i =0;i<n;i++)
+        // {
+        // int temp=goldmineM(i,0,grid,dp);
+        // if(temp>ans) ans=temp;
+        // }
 
-        System.out.println(ans);
+        System.out.println(goldmineT(grid, dp));
 
     }
 }
