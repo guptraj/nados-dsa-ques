0755a49d457ed392525ad5df95a6c4d8711111bc
Rajat Gupta <90345945+guptraj@users.noreply.github.com>
2022-12-18 15:37:38 +0530

    added

---
+++ 09. HashMap/Sort K-sorted Array/Main.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      int[] arr = new int[n];
+
+      for (int i = 0; i < n; i++) {
+         arr[i] = Integer.parseInt(br.readLine());
+      }
+
+      int k = Integer.parseInt(br.readLine());
+      PriorityQueue<Integer> pq= new PriorityQueue<>();
+            
+      for(int i=0;i<n;i++)
+      {
+         if(i<k){
+            pq.add(arr[i]);
+         }
+         else{
+            pq.add(arr[i]);
+            System.out.println(pq.peek());
+            pq.remove();
+         }
+      }
+      while(pq.size() != 0){
+         System.out.println(pq.peek());
+         pq.remove();
+      }
+   }
+
+}
